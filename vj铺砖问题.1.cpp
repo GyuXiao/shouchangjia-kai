4743e5674e7e78f4c7ac579d567b5a00713f559d
lightyears <lightyears1998@hotmail.com>
2017-11-21 23:10:34 +0800

    在一行之内解决问题
    其他格式建议：
    
    1. 在文件末尾保留一行空行。
    2. 操作符与操作数之间空格。
    
    : )

--- vj铺砖问题.1.cpp
+++ vj铺砖问题.1.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-    long long n, m, a,  b, c,d;
-    while(cin>>n>>m>>a)
+    long long n, m, a, b, c, d;
+    while (cin >> n >> m >> a)
     {
-        if(n%a==0)b=n/a;
-        else b=n/a+1;
-        if(m%a==0)c=m/a;
-        else c= m/a+1;
-        d=b*c;
-        cout<< d<<endl;
+        (n % a) ? (b = n / a + 1) : (b = n / a);
+        (m % a) ? (c = m / a + 1) : (c = m / a);
+        d = b * c;
+        cout << d << endl;
     }
 }
